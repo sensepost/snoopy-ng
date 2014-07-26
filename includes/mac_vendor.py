02ccdf41c84ef932b79c0f3939e5bc76503412df
Maxim Chernyshev <maximcherny@gmail.com>
2014-07-26 17:30:22 +0800

    Added the ability to update mac_vendor.txt based on Wireshark manuf data

--- includes/mac_vendor.py
+++ includes/mac_vendor.py
@@ -18,3 +18,57 @@ class mac_vendor():
             return self.mac_lookup[mac]
         else:
             return ("Unknown", "Unknown device")
+
+if __name__ == "__main__":
+    import sys, argparse, requests, re
+    from urlparse import urlparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--update", help="Update OUI data from known remote IEEE text file", action='store_true')
+    parser.add_argument("-f", "--file", help="The location of oui.txt file")
+    args = parser.parse_args()
+
+    if not args.update:
+        print "[!] No operation specified! Try --help."
+        sys.exit(-1)
+
+    if not args.file:
+        args.file = "https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD"
+
+    o = urlparse(args.file)
+
+    print "[+] Fetching data from %s..."%args.file
+
+    if not o.scheme or o.scheme == "file":
+        with open(args.file, "r") as f:
+            data = f.read()
+    elif o.scheme == "http" or o.scheme == "https":
+        r = requests.get(args.file)
+        data = r.text.encode("utf8")
+    else:
+        print "[!] Only local files or http(s) URLs are supported."
+        sys.exit(-1)
+
+    count = 0
+    f = open("%s/mac_vendor.txt"%path, "w")
+    for line in data.split('\n'):
+        try:
+            mac, vendor = re.search(r'([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})\t(.*)', line).groups()
+            vendor = vendor.split("# ")
+            vendorshort = vendor[0].strip()
+            vendorlong = vendorshort
+            if (len(vendor) == 2):
+                vendorlong = vendor[1].strip()
+            f.write("|".join((mac.replace(":", "").upper(), vendorshort, vendorlong + "\n")))
+            count += 1
+        except AttributeError:
+            continue
+        except:
+            print "[!] Processing error - you may need to restore mac_vendor.txt manually."
+            sys.exit(-1)
+
+    f.close()
+    print "[+] Wrote %d OUI entries"%count
+
+
+
